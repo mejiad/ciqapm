3dee14c13105f29fca63e33040c524286faba9fe
mejiad <mejiad@evol-tech.com>
2024-09-17 21:05:01 -0600

    Init commit

---
+++ src/main/java/com/evoltech/ciqapm/CiqapmApplication.java
@@ -0,0 +1,13 @@
+package com.evoltech.ciqapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiqapmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiqapmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evoltech/ciqapm/CiqapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoltech.ciqapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiqapmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
