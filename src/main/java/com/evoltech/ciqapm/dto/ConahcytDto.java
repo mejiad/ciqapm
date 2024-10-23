29a18d3d2ca30e9aceb171a575b0ce9fc9808e66
José Daniel Mejía Sánchez <mejiad@evol-tech.com>
2024-10-22 20:38:12 -0600

    Cambios en el view de conahcyt

--- src/main/java/com/evoltech/ciqapm/dto/ConahcytDto.java
+++ src/main/java/com/evoltech/ciqapm/dto/ConahcytDto.java
@@ -44,7 +44,7 @@ public class ConahcytDto {
         Proyecto proyecto = new Proyecto();
         proyecto.setNombre(nombre);
 
-        proyecto.setDescripcion(descripcion);
+        proyecto.setDescripcion(this.descripcion);
 
         proyecto.setEstatus(Estado.PROCESO);
 
@@ -66,7 +66,7 @@ public class ConahcytDto {
     public ConahcytDto(Proyecto proyecto, DatosConahcyt datosConahcyt) {
         this.id = proyecto.getId();
         this.nombre = proyecto.getNombre();
-        this.descripcion = proyecto().getDescripcion();
+        this.descripcion = proyecto.getDescripcion();
         this.estatus = proyecto.getEstatus();
         this.responsable = proyecto.getResponsable();
         this.convocatoria = datosConahcyt.getConvocatoria();
--- src/main/java/com/evoltech/ciqapm/repository/datos/ConahcytRepository.java
+++ src/main/java/com/evoltech/ciqapm/repository/datos/ConahcytRepository.java
@@ -3,10 +3,13 @@ package com.evoltech.ciqapm.repository.datos;
 import com.evoltech.ciqapm.model.Proyecto;
 import com.evoltech.ciqapm.model.TipoProyecto;
 import com.evoltech.ciqapm.model.datos.DatosConahcyt;
+import com.evoltech.ciqapm.model.datos.DatosIndustria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ConahcytRepository extends JpaRepository<DatosConahcyt, Long> {
+
+    DatosConahcyt findByProyecto(Proyecto proyecto);
 
 }
