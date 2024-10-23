fff5f44af23e59f302d6ed5092032b7fbe499c05
mejiad <mejiad@evol-tech.com>
2024-10-23 10:01:51 -0600

    Alumnos edit

--- src/main/java/com/evoltech/ciqapm/controllers/AlumnoController.java
+++ src/main/java/com/evoltech/ciqapm/controllers/AlumnoController.java
@@ -2,11 +2,15 @@ package com.evoltech.ciqapm.controllers;
 
 import com.evoltech.ciqapm.model.Alumno;
 import com.evoltech.ciqapm.repository.AlumnoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +27,35 @@ public class AlumnoController {
         model.addAttribute("alumnos", alumnos);
 
         return "/Alumno/List";
+    }
+
+
+    @GetMapping("/new")
+    public String newAlumno(Model model){
+        Alumno alumno = new Alumno();
+        model.addAttribute("alumno", alumno);
+
+        return "/Alumno/Edit";
+    }
+
+    @PostMapping("/save")
+    public String saveAlumno(@Valid Alumno alumno, BindingResult result, Model model){
+        if (result.hasErrors()){
+            System.out.println("Hay errores");
+            return "/Alumno/Edit";
+        } else {
+            alumnoRepository.save(alumno);
+            List<Alumno> alumnos = alumnoRepository.findAll();
+            model.addAttribute("alumnos", alumnos);
+            return "Alumno/List";
+        }
+    }
+
+    @GetMapping("/edit")
+    public String editAlumno(@RequestParam("id") Long id, Model model){
+        Alumno alumno = alumnoRepository.getReferenceById(id);
+
+        model.addAttribute("alumno", alumno);
+        return "Alumno/Edit";
     }
 }
--- src/main/java/com/evoltech/ciqapm/model/Alumno.java
+++ src/main/java/com/evoltech/ciqapm/model/Alumno.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -20,12 +21,15 @@ public class Alumno {
     private Long id;
 
     @NotNull(message = "El nombre debe ser digitado.")
+    @NotEmpty (message = "El nombre debe ser digitado.")
     private String nombre;
 
     @NotNull(message = "La matricula debe ser digitada.")
+    @NotEmpty(message = "La matricula debe ser digitada.")
     private String matricula;
 
     @NotNull(message = "El email debe ser digitado.")
+    @NotEmpty(message = "El email debe ser digitado.")
     private String email;
 
 }
