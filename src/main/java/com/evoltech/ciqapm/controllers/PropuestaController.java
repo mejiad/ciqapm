c3f5786b4b6c0978e473b2e95dfcf1152d7fef6a
mejiad <mejiad@evol-tech.com>
2024-11-08 15:33:24 -0600

    Propuesta controller

--- src/main/java/com/evoltech/ciqapm/controllers/PropuestaController.java
+++ src/main/java/com/evoltech/ciqapm/controllers/PropuestaController.java
@@ -1,7 +1,10 @@
 package com.evoltech.ciqapm.controllers;
 
+import com.evoltech.ciqapm.model.Estado;
+import com.evoltech.ciqapm.model.Personal;
 import com.evoltech.ciqapm.model.Propuesta;
 import com.evoltech.ciqapm.model.Proyecto;
+import com.evoltech.ciqapm.repository.PersonalRepository;
 import com.evoltech.ciqapm.repository.PropuestaRepository;
 import com.evoltech.ciqapm.repository.ProyectoRepository;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
@@ -26,13 +29,20 @@ public class PropuestaController {
     @Autowired
     ProyectoRepository proyectoRepository;
 
+    @Autowired
+    PersonalRepository personalRepository;
+
     @GetMapping("list")
     private String lista(@RequestParam("id") Long id, Model model){
         Proyecto proyecto = proyectoRepository.getReferenceById(id);
         List<Propuesta> propuestas = propuestaRepository.findByProyecto(proyecto);
+        List<Personal> personas = personalRepository.findAll();
+        List<Estado> estados = List.of(Estado.values());
 
+        model.addAttribute("personas", personas);
         model.addAttribute("propuestas", propuestas);
         model.addAttribute("proyecto", proyecto);
+
 
         return "Propuesta/List";
     }
@@ -42,7 +52,11 @@ public class PropuestaController {
         Proyecto proyecto = proyectoRepository.getReferenceById(id);
 
         Propuesta propuesta = new Propuesta();
+        List<Personal> personas = personalRepository.findAll();
+        List<Estado> estados = List.of(Estado.values());
 
+        model.addAttribute("estados", estados);
+        model.addAttribute("personas", personas);
         model.addAttribute("propuesta", propuesta);
         model.addAttribute("proyecto", proyecto);
 
@@ -52,10 +66,10 @@ public class PropuestaController {
     @PostMapping("save")
     private String salvar(Propuesta propuesta, BindingResult result, Model model) {
 
-       Propuesta res = propuestaRepository.save(propuesta) ;
+       Propuesta res = propuestaRepository.save(propuesta);
+        System.out.println("Regresando a:" + res.getProyecto().getNombre());
 
-       return "redirect:conahcyt/view/" + res.getProyecto().getId();
-
+       return "redirect:/conahcyt/view/" + res.getProyecto().getId();
     }
 
 }
