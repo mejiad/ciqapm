0ec774e81a807841e617bd43886c647fb3e16eea
mejiad <mejiad@evol-tech.com>
2024-11-14 08:58:38 -0600

    fix merge pagos.java

--- src/main/java/com/evoltech/ciqapm/model/Entregable.java
+++ src/main/java/com/evoltech/ciqapm/model/Entregable.java
@@ -1,16 +1,8 @@
 package com.evoltech.ciqapm.model;
 
-<<<<<<< HEAD
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jdk.jfr.MetadataDefinition;
-=======
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
->>>>>>> entregables-etapa
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
